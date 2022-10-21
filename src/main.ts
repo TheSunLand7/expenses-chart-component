5f20df81b4dabba47b368a2586694a7fdac6d2f8
joshub <jos.ct101@gmail.com>
2022-10-21 11:52:03 -0500

    Desktop design and logic [Done]
    Signed-off-by: joshub <jos.ct101@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-console.log("Typescript here!");
+import data from "./data.json";
+import "./style.css";
 
-export {};
+const $dayShort = document.querySelectorAll(".day span") as NodeListOf<HTMLSpanElement>;
+const showChart = () => {
+	let currentDay: string = new Intl.DateTimeFormat("en-US", { weekday: "long" })
+		.format()
+		.toLowerCase(); //We get friday for example
+	const $dayLong = document.getElementById(`${currentDay}`)!
+		.previousElementSibling as HTMLDivElement;
+	$dayShort.forEach((item, index) => {
+		let sibling = item.previousElementSibling as HTMLDivElement;
+		sibling.style.height = `${data[index].amount}%`;
+	});
+	$dayLong.style.backgroundColor = "var(--cyan)";
+};
+
+window.onload = () => {
+	showChart();
+};
